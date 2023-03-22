3a18f01c6d0aea7ddd7007e9c3c75f16d7e4c2a4
THANG NGUYEN <thangnguyen24111990@gmail.com>
2023-03-22 18:16:29 +0200

    Add favicon

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -7,7 +7,7 @@ type Props = {
 
 export default function Layout({ children }: Props) {
   return (
-    <div className="min-h-screen myTheme" data-theme={'dark'}>
+    <div className="min-h-screen myTheme" data-theme={'light'}>
       <div className="container py-8 mx-auto ">
         <Header />
         <main role="main" aria-label="Main Content">
