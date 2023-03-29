e9dd0239e2eb243c1ae416fde886035c96fdb79f
THANG NGUYEN <thangnguyen24111990@gmail.com>
2023-03-30 02:58:32 +0300

    Add admin page with CRUD functions

---
+++ src/admin/components/Popup/Popup.tsx
@@ -0,0 +1,5 @@
+type Props = {}
+
+export default function Popup() {
+  return <div>Popup</div>
+}
--- src/admin/pages/Admin/Admin.tsx
+++ src/admin/pages/Admin/Admin.tsx
@@ -1,59 +1,60 @@
+import BookItem from '@/admin/components/BookItem/BookItem'
+import Popup from '@/admin/components/Popup/Popup'
+import Loading from '@/components/Loading/Loading'
+import { addNewBook, fetchBooks } from '@/redux/features/book/booksSlice'
+import { RootState, useAppDispatch } from '@/redux/store'
+import { IBook } from '@/types'
+import { useEffect } from 'react'
+import { useSelector } from 'react-redux'
+import { v4 as uuidv4 } from 'uuid'
+
 export default function Admin() {
-  // const bookChange: Omit<IBook, 'id'> = {
-  //   picture: 'https://picsum.photos/id/3/200/300.webp',
-  //   isbn: '9789-0-6-0',
-  //   title: 'nulla fugiat sint',
-  //   description:
-  //     'Irure excepteur aliqua minim et minim. Anim deserunt nisi eu sunt commodo aliquip ut velit.',
-  //   author: '2',
-  //   category: '1',
-  //   publisher: '10',
-  //   status: 'borrowed',
-  //   publishedDate: '1971-02-08'
-  // }
+  const dispatch = useAppDispatch()
+  const books = useSelector((state: RootState) => state.books.books)
+  const status = useSelector((state: RootState) => state.books.status)
 
-  // const handleRemoveBook = (id: string) => {
-  //   dispatch(removeBook(id))
-  // }
+  useEffect(() => {
+    dispatch(fetchBooks())
+  }, [])
 
-  // const handleUpdateBook = (id: string, newData: Omit<IBook, 'id'>) => {
-  //   dispatch(updateBook({ id, newData }))
-  // }
+  const newBook: IBook = {
+    id: uuidv4(),
+    picture: 'https://picsum.photos/id/19/200/300.webp',
+    isbn: '9780-9-0-9',
+    title: 'exercitation incididunt commodo',
+    description:
+      'In esse adipisicing ad voluptate magna. Non mollit ut exercitation tempor ea irure dolore duis mollit.',
+    publisher: '6',
+    author: '12',
+    category: '4',
+    status: 'available',
+    publishedDate: '1980-10-25'
+  }
 
-  // const newBook: IBook = {
-  //   id: uuidv4(),
-  //   picture: 'https://picsum.photos/id/19/200/300.webp',
-  //   isbn: '9780-9-0-9',
-  //   title: 'exercitation incididunt commodo',
-  //   description:
-  //     'In esse adipisicing ad voluptate magna. Non mollit ut exercitation tempor ea irure dolore duis mollit.',
-  //   publisher: '6',
-  //   author: '12',
-  //   category: '4',
-  //   status: 'available',
-  //   publishedDate: '1980-10-25'
-  // }
+  const handleAddBook = (book: IBook) => {
+    dispatch(addNewBook(book))
+  }
 
-  // const handleSearch = useCallback(
-  //   (searchTerm: string) => {
-  //     dispatch(
-  //       searchBooks({
-  //         searchTerm: searchTerm,
-  //         keysToSearch: ['title']
-  //       })
-  //     )
-  //   },
-  //   [dispatch]
-  // )
-  // const handleAddBook = (book: IBook) => {
-  //   dispatch(addNewBook(book))
-  // }
-  // const handleSort = useCallback(
-  //   (field: keyof IBook, order: 'asc' | 'desc') => {
-  //     dispatch(sortBooks({ field, order }))
-  //   },
-  //   [dispatch]
-  // )
+  {
+    status === 'loading' ? <Loading classes="mx-auto" /> : null
+  }
 
-  return <div>Admin</div>
+  return (
+    <>
+      <div className="container p-4 mx-auto">
+        <h1 className="text-4xl text-center">Dashboard</h1>
+        <p>
+          <button className="button button-outlined" onClick={() => handleAddBook(newBook)}>
+            Add new book
+          </button>
+        </p>
+        <div className="grid grid-cols-6 gap-4 mt-8">
+          {books.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </div>
+      </div>
+      <Popup />
+    </>
+  )
 }
